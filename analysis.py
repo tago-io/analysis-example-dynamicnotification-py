12526b4d6c70f0151355df7c13b8d933d0582a7d
mateuscardosodeveloper <mateus.informatica44@gmail.com>
2023-11-09 11:02:21 -0300

    Updated code to new version of sdk

--- analysis.py
+++ analysis.py
@@ -17,7 +17,7 @@ Steps to generate an account_token:
 4 - Generate a new Token with Expires Never.
 5 - Press the Copy Button and place at the Environment Variables tab of this analysis.
 """
-from tagoio_sdk import Analysis, Account, Services
+from tagoio_sdk import Analysis, Resources, Services
 from tagoio_sdk.modules.Utils.envToJson import envToJson
 
 
@@ -28,78 +28,57 @@ def my_analysis(context, scope: list[dict]) -> None:
     # Get the environment variables.
     environment_variables = envToJson(context.environment)
 
-    if environment_variables.get("account_token"):
+    if not environment_variables.get("account_token"):
         return print('Missing "account_token" environment variable')
     elif len(environment_variables["account_token"]) != 36:
         return print('Invalid "account_token" in the environment variable')
 
-    # Instance the Account class
-    account = Account({"token": environment_variables["account_token"]})
+    # Instance the Resources class
+    resources = Resources()
 
     # Get the device ID from the scope and retrieve device information.
     device_id = scope[0]["device"]
-    device_info = account.devices.info(device_id)
+    device_info = resources.devices.info(device_id)
+    device_name = device_info["name"]
 
     # Get the device name and tags from the device.
     # [TAG KEY]    [TAG VALUE]
     # email        example@tago.io
     # phone        +1XXxxxxxxx
     # user_id      5f495ae55ff03d0028d39fc5
-    #
+    email_tag = next(filter(lambda tag: tag["key"] == "email", device_info["tags"]), None)
+    phone_tag = next(filter(lambda tag: tag["key"] == "phone", device_info["tags"]), None)
+    user_id_tag = next(filter(lambda tag: tag["key"] == "user_id", device_info["tags"]), None)
+
     # This is just a generic example how to get this information. You can get data from a device, search in tags, or any other way of correlation you have.
     # For example, you can get the email directly from the user_id if it was specified:
-    # email = await account.run.user_info(user_id_tag["id"])["email"]
-    device_name = device_info["name"]
-    email_tag = next(
-        (tag for tag in device_info["tags"] if tag["key"] == "email"), None
-    )
-    phone_tag = next(
-        (tag for tag in device_info["tags"] if tag["key"] == "phone"), None
-    )
-    user_id_tag = next(
-        (tag for tag in device_info["tags"] if tag["key"] == "user_id"), None
-    )
+    # email = await resources.run.user_info(user_id_tag["value"]).get("email")
 
     # Instance the SMS and Email service using the analysis token from the context.
     email_service = Services({"token": context.token}).email
     sms_service = Services({"token": context.token}).sms
 
     # Send the notifications and output the results to the analysis console.
+    message = f"You received a notification for the device: {device_name}. Variable: {scope[0]['variable']}, Value: {scope[0]['value']}"
+
     if email_tag:
-        result = email_service.send(
-            {
-                "to": email_tag["value"],
-                "subject": "Notification alert",
-                "message": f"You received a notification for the device: {device_name}. Variable: {scope[0]['variable']}, Value: {scope[0]['value']}",
-            }
-        )
+        result = email_service.send({"to": email_tag["value"], "subject": "Notification alert", "message": message})
         print(result)
     else:
         print("Email not found for this device.")
 
     if phone_tag:
-        result = sms_service.send(
-            {
-                "to": phone_tag["value"],
-                "message": f"You received a notification for the device: {device_name}. Variable: {scope[0]['variable']}, Value: {scope[0]['value']}",
-            }
-        )
+        result = sms_service.send({"to": phone_tag["value"], "message": message,})
         print(result)
     else:
         print("Phone number not found for this device.")
 
     if user_id_tag:
-        result = account.run.notificationCreate(
-            user_id_tag["value"],
-            {
-                "title": "Notification Alert",
-                "message": f"You received a notification for the device: {device_name}. Variable: {scope[0]['variable']}, Value: {scope[0]['value']}",
-            },
-        )
+        result = resources.run.notificationCreate(user_id_tag["value"], {"title": "Notification Alert", "message": message})
         print(result)
     else:
         print("User ID not found for this device.")
 
 
 # The analysis token in only necessary to run the analysis outside TagoIO
-Analysis(params={"token": "MY-ANALYSIS-TOKEN-HERE"}).init(my_analysis)
+Analysis.use(my_analysis, params={"token": "MY-ANALYSIS-TOKEN-HERE"})
